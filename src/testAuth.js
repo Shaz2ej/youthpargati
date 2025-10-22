d3e974d79164a0b5742be651aaf7a9ce28f50a14
Shaz2ej <youthpargatii@gmail.com>
2025-10-22 13:12:34 -0700

    Clean up project: Remove Supabase/test/debug/extra files and duplicate video components

--- src/testAuth.js
+++
@@ -1,7 +0,0 @@
-// Mock authentication test function
-export const testAuth = async () => {
-  console.log('Testing authentication...');
-  
-  // Mock authentication data
-  console.log('Authentication system is not available.');
-};
